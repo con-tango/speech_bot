cb7a57aae0f22347559c139c1d4dd614a3db9129
Dmytro Shylko <schilcault@aol.com>
2023-07-17 14:09:04 +0300

    commenting subprocess.run arguments

--- voice.py
+++ voice.py
@@ -6,6 +6,8 @@ rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-60)
 engine.setProperty("voice", '[] HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
 
+
+
 # ffmpeg -i test.mp3 -c:a libopus test_out.ogg
 # ["ffmpeg", '-i', audio_path_wav, '-acodec', 'libopus', audio_path_ogg, '-y']
 
@@ -18,5 +20,7 @@ def text_to_file(text):
     engine.runAndWait()
     subprocess.run(["ffmpeg", '-i', mp3_file, '-acodec', 'libopus', out_file, '-y'])
     return out_file
+
+
 
 # text_to_file('Hi, I\'m a message')
